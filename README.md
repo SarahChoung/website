31756b8b94a6a76f7e97d5ea800a5d24f6de7af5
Sarah Choung <sarahjchoung@gmail.com>
2020-07-24 22:46:45 +0000

    Starter Files Added

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# website
-
-My website
+## Website
---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom/cjs/react-dom.development';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <h1>Test</h1>
+    )
+  }
+}
