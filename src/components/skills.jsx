9074e1b17ff4d4d8a75b10a2ec24784034690e8d
Sarah Choung <sarahjchoung@gmail.com>
2020-07-28 19:59:49 +0000

    added skills svg assets

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -4,6 +4,9 @@ export default function Skills(props) {
   return (
     <div id="skills" className="section">
       <h1>Skills</h1>
+      <object data="images/skills/css.svg" type="image/svg+xml">
+        <img src="yourfallback.jpg" />
+      </object>
     </div>
   )
 }
