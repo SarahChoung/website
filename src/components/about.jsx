bdeafd764fe7a157a1ac005c77ed5d13ea86d8b7
Sarah Choung <sarahjchoung@gmail.com>
2020-07-30 04:02:39 +0000

    basic about me skeleton added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function About(props) {
   return (
-    <div id="about" className="section">
-      <h1>About Me</h1>
+    <div id="about" className="section text-center d-flex flex-column justify-content-center">
+      <div className="pb-5">
+        <h1>About Me</h1>
+      </div>
+      <div className="d-flex justify-content-end w-75 m-auto">
+        <div className="row">
+          <div className="col col-lg-6 mb-5">
+            <img src="/images/profile-photo.jpg" alt="profile picture"></img>
+          </div>
+          <div className="col col-lg-6">
+            <h3>Hi, I'm Sarah</h3>
+            <p>About me blurb. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptas, rem id, eaque unde ducimus vero iure commodi numquam itaque aliquam similique quae dolorum quo. Perspiciatis et repellat aliquam eligendi?</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-export default function Contact(props) {
-  return (
-    <div id="contact" className="section">
-      <h1>Contact</h1>
-    </div>
-  )
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <div id="contact" className="section text-center d-flex flex-column justify-content-center">
+        <h1>Contact</h1>
+        <div className="container p-3">
+          <div className="row">
+            <div className="col-4">
+              <h6>Location</h6>
+              <p>Orange County, CA</p>
+            </div>
+            <div className="col-4">
+              <h6>Mobile</h6>
+              <a href="tel:+17147320972">&#40;714&#41; 732-0972</a>
+            </div>
+            <div className="col-4">
+              <h6>Email</h6>
+              <a href="mailto: sarahjchoung@gmail.com">sarahjchoung&#64;gmail.com</a>
+            </div>
+          </div>
+        </div >
+      </div >
+    )
+  }
 }
