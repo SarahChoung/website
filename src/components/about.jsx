5791a173660444427122d59b7e970236ef5b20e8
Sarah Choung <sarahjchoung@gmail.com>
2020-07-30 23:11:03 +0000

    Added links to fa icons

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ export default function About(props) {
       <div className="pb-5">
         <h1>About Me</h1>
       </div>
-      <div className="d-flex justify-content-end w-75 m-auto">
+      <div className="d-flex justify-content-center w-75 m-auto">
         <div className="row">
           <div className="col col-lg-6 mb-5">
             <img src="/images/profile-photo.jpg" alt="profile picture"></img>
@@ -14,12 +14,12 @@ export default function About(props) {
           <div className="col col-lg-6">
             <h3>Hi, I'm Sarah</h3>
             <p className="text-left">About me blurb. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptas, rem id, eaque unde ducimus vero iure commodi numquam itaque aliquam similique quae dolorum quo. Perspiciatis et repellat aliquam eligendi?</p>
-            <div>
-              <i className="fa fa-lg fa-envelope"></i>
-              <i className="fa fa-lg fa-phone"></i>
-              <i className="fa fa-lg fa-file"></i>
-              <i className="fab fa-lg fa-github"></i>
-              <i className="fab fa-lg fa-linkedin"></i>
+            <div className="d-flex justify-content-around w-50 m-auto">
+              <a href="mailto: sarahjchoung@gmail.com"><i className="fa fa-lg fa-envelope"></i></a>
+              <a href="tel:+17147320972"><i className="fa fa-lg fa-phone"></i></a>
+              <a href="/images/resume.pdf"><i className="fa fa-lg fa-file"></i></a>
+              <a href="https://github.com/SarahChoung"><i className="fab fa-lg fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/sarah-choung/"><i className="fab fa-lg fa-linkedin"></i></a>
             </div>
           </div>
         </div>
--- src/components/applications.jsx
+++ src/components/applications.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Applications(props) {
   return (
-    <div id="applications" className="section">
+    <div id="applications" className="section text-center d-flex flex-column justify-content-center">
       <h1>Applications</h1>
     </div>
   )
