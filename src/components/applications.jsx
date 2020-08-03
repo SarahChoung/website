9b1e5a0ba3b7521ef88b945d6b643b54892dc01d
Sarah Choung <sarahjchoung@gmail.com>
2020-08-03 22:37:20 +0000

    deleted unneeded scripts, added smooth scroll

--- src/components/applications.jsx
+++ src/components/applications.jsx
@@ -9,38 +9,38 @@ export default function Applications(props) {
           <div className="col-12 col-md-4 app-card">
             <img className="app-image" src="/images/art4bid.png" alt="art4bid app"></img>
             <div className="w-100 d-flex flex-wrap pt-1">
-              <span className="badge badge-pill badge-secondary">{`JavaScript (ES5/ES6)`}</span>
-              <span className="badge badge-pill badge-secondary">React.js</span>
-              <span className="badge badge-pill badge-secondary">Node.js</span>
-              <span className="badge badge-pill badge-secondary">HTML5</span>
-              <span className="badge badge-pill badge-secondary">CSS3</span>
-              <span className="badge badge-pill badge-secondary">PostgreSQL</span>
-              <span className="badge badge-pill badge-secondary">Express</span>
-              <span className="badge badge-pill badge-secondary">Bootstrap 4</span>
+              <span className="badge-class badge badge-pill badge-dark">{`JavaScript (ES5/ES6)`}</span>
+              <span className="badge-class badge badge-pill badge-dark">React.js</span>
+              <span className="badge-class badge badge-pill badge-dark">Node.js</span>
+              <span className="badge-class badge badge-pill badge-dark">HTML5</span>
+              <span className="badge-class badge badge-pill badge-dark">CSS3</span>
+              <span className="badge-class badge badge-pill badge-dark">PostgreSQL</span>
+              <span className="badge-class badge badge-pill badge-dark">Express</span>
+              <span className="badge-class badge badge-pill badge-dark">Bootstrap 4</span>
             </div>
           </div>
           <div className="col-12 col-md-4 app-card">
-            <img className="app-image" src="/images/light-box.png" alt="light-box app"></img>
+            <img className="app-image" src="/images/light-box.png" alt="dark-box app"></img>
             <div className="w-100 d-flex flex-wrap pt-1">
-              <span className="badge badge-pill badge-secondary">{`JavaScript (ES5/ES6)`}</span>
-              <span className="badge badge-pill badge-secondary">React.js</span>
-              <span className="badge badge-pill badge-secondary">Node.js</span>
-              <span className="badge badge-pill badge-secondary">HTML5</span>
-              <span className="badge badge-pill badge-secondary">CSS3</span>
-              <span className="badge badge-pill badge-secondary">Express</span>
-              <span className="badge badge-pill badge-secondary">Bootstrap 4</span>
+              <span className="badge-class badge badge-pill badge-dark">{`JavaScript (ES5/ES6)`}</span>
+              <span className="badge-class badge badge-pill badge-dark">React.js</span>
+              <span className="badge-class badge badge-pill badge-dark">Node.js</span>
+              <span className="badge-class badge badge-pill badge-dark">HTML5</span>
+              <span className="badge-class badge badge-pill badge-dark">CSS3</span>
+              <span className="badge-class badge badge-pill badge-dark">Express</span>
+              <span className="badge-class badge badge-pill badge-dark">Bootstrap 4</span>
             </div>
           </div>
           <div className="col-12 col-md-4 app-card">
             <img className="app-image" src="/images/movies-to-see.png" alt="movies-to-see app"></img>
             <div className="w-100 d-flex flex-wrap pt-1">
-              <span className="badge badge-pill badge-secondary">{`JavaScript (ES5/ES6)`}</span>
-              <span className="badge badge-pill badge-secondary">HTML5</span>
-              <span className="badge badge-pill badge-secondary">CSS3</span>
-              <span className="badge badge-pill badge-secondary">jQuery</span>
-              <span className="badge badge-pill badge-secondary">AJAX</span>
-              <span className="badge badge-pill badge-secondary">REST API</span>
-              <span className="badge badge-pill badge-secondary">Bootstrap 4</span>
+              <span className="badge-class  badge badge-pill badge-dark">{`JavaScript (ES5/ES6)`}</span>
+              <span className="badge-class badge badge-pill badge-dark">HTML5</span>
+              <span className="badge-class badge badge-pill badge-dark">CSS3</span>
+              <span className="badge-class badge badge-pill badge-dark">jQuery</span>
+              <span className="badge-class badge badge-pill badge-dark">AJAX</span>
+              <span className="badge-class badge badge-pill badge-dark">REST API</span>
+              <span className="badge-class badge badge-pill badge-dark">Bootstrap 4</span>
             </div>
           </div>
         </div>
