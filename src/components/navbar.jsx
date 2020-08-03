be067f2c56d957e0a4a2ef1da3955919d32802ed
Sarah Choung <sarahjchoung@gmail.com>
2020-08-03 19:38:11 +0000

    Basic Bootstrap responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,20 +6,25 @@ export default class NavBar extends React.Component {
       <div id="#top">
         <div id="header-placeholder">&nbsp;</div>
         <div id="header-container" className="fixed-top">
-          <header className="d-flex justify-content-between m-auto align-items-end p-2">
+          <nav className="header navbar navbar-expand-lg navbar-light d-flex justify-content-between m-auto p-2">
             <div>
-              <h1 className="m-0">Placeholder</h1>
+              <a class="navbar-brand" href="#">Placeholder</a>
             </div>
-            <ul id="nav-sections" className="d-flex w-50 justify-content-around">
-              <li><a href="#top">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#applications">Applications</a></li>
-              <li><a href="#skills">Skills</a></li>
-              <li><a href="#tools">Tools</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </header>
-        </div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" href="#">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse mr-auto" id="navbarNav">
+              <ul className="navbar-nav" id="nav-sections">
+                <li className="nav-item active" ><a className="nav-link" href="#top">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+                <li className="nav-item"><a className="nav-link" href="#applications">Applications</a></li>
+                <li className="nav-item"><a className="nav-link" href="#skills">Skills</a></li>
+                <li className="nav-item"><a className="nav-link" href="#tools">Tools</a></li>
+                <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div >
       </div >
     )
   }
