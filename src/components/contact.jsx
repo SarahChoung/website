4156e0c18d49fa8ad1a791e51cd0868e7a97eaa1
Sarah Choung <sarahjchoung@gmail.com>
2020-08-13 12:07:36 -0700

    Added contact icons

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,15 +9,15 @@ export default class Contact extends React.Component {
           <div className="row">
             <div className="col-12 col-md-4">
               <h6>Location</h6>
-              <p>Orange County, CA</p>
+              <p><i className="icon fa fa-map-marker-alt mr-1"></i>Orange County, CA</p>
             </div>
             <div className="col-12  col-md-4">
               <h6>Mobile</h6>
-              <a href="tel:+17147320972">&#40;714&#41; 732-0972</a>
+              <a href="tel:+17147320972"><i className="icon fa fa-phone mr-1"></i>&#40;714&#41; 732-0972</a>
             </div>
             <div className="col-12 col-md-4">
               <h6>Email</h6>
-              <a href="mailto: sarahjchoung@gmail.com">sarahjchoung&#64;gmail.com</a>
+              <a href="mailto: sarahjchoung@gmail.com"><i className="icon fa fa-envelope mr-1"></i>sarahjchoung&#64;gmail.com</a>
             </div>
           </div>
         </div >
