2444e323c6d51b3d5bc9535e97db745695502200
Sarah Choung <sarahjchoung@gmail.com>
2020-08-17 22:21:53 -0700

    responsive navbar highlights work

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,18 +9,96 @@ import Tools from './tools';
 import Contact from './contact'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleSection: 'Home'
+    }
+    this.navBarRef = React.createRef()
+    this.homeRef = React.createRef()
+    this.aboutRef = React.createRef()
+    this.applicationsRef = React.createRef()
+    this.skillsRef = React.createRef()
+    this.toolsRef = React.createRef()
+    this.contactRef = React.createRef()
+    this.getDimensions = this.getDimensions.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
+    this.sectionRefs
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+    this.sectionRefs = [
+      { section: "Home", ref: this.homeRef },
+      { section: "About", ref: this.aboutRef },
+      { section: "Applications", ref: this.applicationsRef },
+      { section: "Skills", ref: this.skillsRef },
+      { section: "Tools", ref: this.toolsRef },
+      { section: "Contact", ref: this.contactRef },
+    ]
+  }
+
+  handleScroll() {
+    const { height: navBarHeight } = this.getDimensions(this.navBarRef.current);
+    const scrollPosition = window.scrollY + navBarHeight
+
+    const selected = this.sectionRefs.find(({ section, ref }) => {
+      const ele = ref.current;
+      if (ele) {
+        const { offsetBottom, offsetTop } = this.getDimensions(ele);
+        return scrollPosition > offsetTop && scrollPosition < offsetBottom;
+      }
+    });
+
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.setState({ visibleSection: 'Contact' });
+    } else if (selected && selected.section !== this.state.visibleSection) {
+      this.setState({ visibleSection: selected.section });
+    } else if (!selected && this.state.visibleSection) {
+      this.setState({ visibleSection: 'Home' });
+    }
+
+  }
+
+  getDimensions(ele) {
+
+    const { height } = ele.getBoundingClientRect();
+    const offsetTop = ele.offsetTop;
+    const offsetBottom = offsetTop + height;
+
+    return {
+      height,
+      offsetTop,
+      offsetBottom,
+    };
+
+  }
 
   render() {
     return (
       <div>
-        <NavBar />
-        <Home />
-        <About />
-        <Applications />
+        <div ref={this.navBarRef}>
+          <NavBar visibleSection={this.state.visibleSection} />
+        </div>
+        <div ref={this.homeRef} >
+          <Home />
+        </div>
+        <div ref={this.aboutRef}>
+          <About />
+        </div>
+        <div ref={this.applicationsRef} >
+          <Applications />
+        </div>
         {/* <MoonLight /> */}
-        <Skills />
-        <Tools />
-        <Contact />
+        <div ref={this.skillsRef} >
+          <Skills />
+        </div>
+        <div ref={this.toolsRef} >
+          <Tools />
+        </div>
+        <div ref={this.contactRef} >
+          <Contact />
+        </div>
       </div>
     )
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,6 +19,8 @@ export default class NavBar extends React.Component {
   }
 
   render() {
+    const { visibleSection } = this.props
+
     let isActive = null;
     let toggle = null;
     if (this.state.active) {
@@ -32,12 +34,12 @@ export default class NavBar extends React.Component {
           <div className="header d-flex justify-content-between align-items-center m-auto p-2">
             <a href="#top"><h2 className="m-0">SC</h2></a>
             <ul className={`nav-links d-flex align-items-center justify-content-around m-0 ${isActive}`}>
-              <li><a href="#top" onClick={this.toggleNav}>Home</a></li>
-              <li><a href="#about" onClick={this.toggleNav}>About</a></li>
-              <li><a href="#applications" onClick={this.toggleNav}>Applications</a></li>
-              <li><a href="#skills" onClick={this.toggleNav}>Skills</a></li>
-              <li><a href="#tools" onClick={this.toggleNav}>Tools</a></li>
-              <li><a href="#contact" onClick={this.toggleNav}>Contact</a></li>
+              <li><a href="#top" onClick={this.toggleNav} className={visibleSection === 'Home' ? 'active-nav' : 'null'}>Home</a></li>
+              <li><a href="#about" onClick={this.toggleNav} className={visibleSection === 'About' ? 'active-nav' : 'null'}>About</a></li>
+              <li><a href="#applications" onClick={this.toggleNav} className={visibleSection === 'Applications' ? 'active-nav' : 'null'}>Applications</a></li>
+              <li><a href="#skills" onClick={this.toggleNav} className={visibleSection === 'Skills' ? 'active-nav' : 'null'}>Skills</a></li>
+              <li><a href="#tools" onClick={this.toggleNav} className={visibleSection === 'Tools' ? 'active-nav' : 'null'}>Tools</a></li>
+              <li><a href="#contact" onClick={this.toggleNav} className={visibleSection === 'Contact' ? 'active-nav' : 'null'}>Contact</a></li>
             </ul>
             <div className="burger" onClick={this.toggleNav} >
               <div className={`${toggle} line1`}></div>
